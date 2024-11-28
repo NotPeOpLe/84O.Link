54ae139987a36d1983fe47a3ae44f98e86b2e9f9
NotPeOpLe <code840@outlook.com>
2024-11-28 15:49:31 +0800

    更新 ESLint 配置，調整 Vue 規則並重構路由邏輯以支持檔案上傳

--- server/routes/index.post.ts
+++ server/routes/index.post.ts
@@ -1,60 +1,69 @@
-export default defineEventHandler(async (event) => {
-  const db = useDrizzle()
-  const form = await readFormData(event)
-  const type = form.get("type")
-  const input = form.get("input")
-  const key = await generateKey()
-  const editKey = await generateEditKey()
+import type { BlobObject } from "@nuxthub/core"
 
-  if (type === "url" && typeof input === "string") {
-    const dbResult = await db
-      .insert(tables.links)
-      .values({ key, type, target: input })
-      .returning({
-        key: tables.links.key,
-        type: tables.links.type,
-        target: tables.links.target,
-        editKey: tables.links.editKey,
+type CreatedLink = {
+  url: string
+  data: Link & { file?: BlobObject }
+}
+
+export default defineEventHandler(
+  async (event): Promise<CreatedLink | undefined> => {
+    const db = useDrizzle()
+    const form = await readFormData(event)
+    const type = form.get("type")
+    const input = form.get("input")
+    const key = await generateKey()
+    const editKey = await generateEditKey()
+
+    if (type === "url" && typeof input === "string") {
+      const dbResult = await db
+        .insert(tables.links)
+        .values({ key, type, target: input })
+        .returning({
+          key: tables.links.key,
+          type: tables.links.type,
+          target: tables.links.target,
+          editKey: tables.links.editKey,
+        })
+
+      return {
+        url: getRequestURL(event) + key,
+        data: dbResult[0],
+      }
+    }
+
+    if (type === "file" && input instanceof File && input.size) {
+      ensureBlob(input, {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error
+        maxSize: "25MB",
+        types: [],
       })
 
-    return {
-      url: getRequestURL(event) + key,
-      data: dbResult[0],
+      const customMetadata: { [x: string]: string } = {}
+      const fileDisablePreview = form.get("file.disablePreview")
+
+      customMetadata.filename = input.name
+      if (fileDisablePreview) customMetadata.disablePreview = "1"
+
+      const file = await hubBlob().put(key, input, { customMetadata })
+
+      const dbResult = await db
+        .insert(tables.links)
+        .values({ key, type, target: key, editKey })
+        .returning({
+          key: tables.links.key,
+          type: tables.links.type,
+          target: tables.links.target,
+          editKey: tables.links.editKey,
+        })
+
+      return {
+        url: getRequestURL(event) + key,
+        data: {
+          ...dbResult[0],
+          file,
+        },
+      }
     }
   }
-
-  if (type === "file" && input instanceof File && input.size) {
-    ensureBlob(input, {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-expect-error
-      maxSize: "25MB",
-      types: [],
-    })
-
-    const customMetadata: { [x: string]: string } = {}
-    const fileDisablePreview = form.get("file.disablePreview")
-
-    customMetadata.filename = input.name
-    if (fileDisablePreview) customMetadata.disablePreview = "1"
-
-    const file = await hubBlob().put(key, input, { customMetadata })
-
-    const dbResult = await db
-      .insert(tables.links)
-      .values({ key, type, target: key, editKey })
-      .returning({
-        key: tables.links.key,
-        type: tables.links.type,
-        target: tables.links.target,
-        editKey: tables.links.editKey,
-      })
-
-    return {
-      url: getRequestURL(event) + key,
-      data: {
-        ...dbResult[0],
-        file,
-      },
-    }
-  }
-})
+)
