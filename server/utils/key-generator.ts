7bef5f65a81a9d0142a7178cd6a27fbd693173ad
840 <code840@outlook.com>
2024-11-28 03:52:20 +0000

    新增 editKey 欄位至 links 表格，並更新相關資料庫遷移與路由邏輯

--- server/utils/key-generator.ts
+++ server/utils/key-generator.ts
@@ -1,5 +1,11 @@
+import crypto from "crypto"
+
 const OFFSET = new Date(2024, 9, 10).getTime()
 
 export async function generateKey() {
   return (Date.now() - OFFSET).toString(36)
+}
+
+export async function generateEditKey() {
+  return crypto.randomBytes(32).toString("hex")
 }
