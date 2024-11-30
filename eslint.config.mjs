a5200acd608b65709aded87ca25669d238d40431
NotPeOpLe <code840@outlook.com>
2024-11-30 14:57:51 +0800

    更新 ESLint 配置

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,4 +5,6 @@ export default withNuxt().overrideRules({
   // Your custom configs here
   "vue/max-attributes-per-line": ["warn", { singleline: 5 }],
   "vue/singleline-html-element-content-newline": "off",
+  "@stylistic/operator-linebreak": "off",
+  "@stylistic/indent-binary-ops": "off",
 })
