d7da95bc2d0334c283422282c25cff59e16a138f
NotPeOpLe <code840@outlook.com>
2024-11-30 14:59:20 +0800

    重構 API 處理邏輯，更新回傳類型，並改善錯誤處理

--- server/api/index.get.ts
+++ server/api/index.get.ts
@@ -1,24 +1,29 @@
-const NOT_FOUND = createError({ statusCode: 404, message: "Not found" })
-
-export default defineEventHandler(async (event): Promise<LinkObject> => {
-  const { key }: { key?: string } = getQuery(event)
-  if (!key) throw NOT_FOUND
-
-  const result = await useDrizzle()
-    .select({
-      key: tables.links.key,
-      type: tables.links.type,
-      target: tables.links.target,
-    })
-    .from(tables.links)
-    .where(eq(tables.links.key, key))
-    .get()
-  if (!result) throw NOT_FOUND
-
-  if (result.type === "file") {
-    const blob = await hubBlob().head(result.target)
-    return Object.assign(result, { file: blob })
-  }
-
-  return result
+const NOT_FOUND = createError({
+  statusCode: 404,
+  message: "Not found",
 })
+
+export default defineEventHandler(
+  async (event): Promise<LinkObjectWithoutEditKey> => {
+    const { key }: { key?: string } = getQuery(event)
+    if (!key) throw NOT_FOUND
+
+    const result = await useDrizzle()
+      .select({
+        key: tables.links.key,
+        type: tables.links.type,
+        target: tables.links.target,
+      })
+      .from(tables.links)
+      .where(eq(tables.links.key, key))
+      .get()
+    if (!result) throw NOT_FOUND
+
+    if (result.type === "file") {
+      const blob = await hubBlob().head(result.target)
+      return Object.assign(result, { file: blob })
+    }
+
+    return result
+  }
+)
